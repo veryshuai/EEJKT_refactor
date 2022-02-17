777ec515efc74e669ab15b17abc2f5635f3f43dd
veryshuai <dj.eco@cbs.dk>
2022-02-17 09:30:36 +0100

    folded some of distance into a new function

--- distance.m
+++ distance.m
@@ -50,43 +50,7 @@ fprintf(' discrete_sim run time: %.2f\n', time2);
 
 error = Data'-Model;
 
-% To exclude intercept of log-log regression, allow next block
-
-    Data(11) = 0;
-%   Model(11) = 0;
-    error(11) = 0;
-    
-
-% To exclude avg. log exports from DANE-based X-D regression, allow next block
-
-    Data(15) = 0;
-%   Model(15) = 0;
-    error(15) = 0;
-    
-% To exclude variance of DANE-based X-D regression, allow next block
-
-   Data(17) = 0;
-%   Model(17) = 0;
-   error(17) = 0;
-    
-% To exclude avg. log domestic sales from DANE-based AR1 regression, allow next block
-
-    Data(18) = 0;
-%   Model(15) = 0;
-    error(18) = 0;
-    
-% To exclude market exit regression, allow next block
-    
-     Data(27:32) = 0;
-     Model(27:32) = 0;
-     error(27:32) = 0;
-    
-% To exclude variance of error in domestic AR1, allow next block  
-    
-  Data(20) = 0;
-%   Model(20) = 0;
-  error(20) = 0;
-%     
+[Data, error, Model] = do_not_match_some_moments(Data, error, Model);
 
 W_D   = log((error'/W)*error);
 Old_D = norm(error)/norm(Data');

98b57ac14ac7b9d6966182e37b514085049f7972
veryshuai <dj.eco@cbs.dk>
2022-02-20 16:27:32 +0100

    fixed (newly added) bug in val_loop_iter

--- solvePolicyHome.m
+++ solvePolicyHome.m
@@ -4,22 +4,22 @@ V = zeros(1,mm.dim1,mm.net_size+1,2*mm.phi_size+1,2*mm.x_size+1);
 %lambda_h (common succ rate (defunct), known succ rate, network size, prod of firm, H macro shock)
 l_opt = zeros(size(V)); 
 
-for j = 1:size(mm.Phi)
+for prod_ind = 1:size(mm.Phi)
     for k = 1:mm.dim1
         
-        l_opt(1,k,mm.net_size+1,j,:) = mm.l_opt_func_h(mm.theta1(k),mm.net_size,policy.pi_h(j,:),0,0,0);
+        l_opt(1,k,mm.net_size+1,prod_ind,:) = mm.l_opt_func_h(mm.theta1(k),mm.net_size,policy.pi_h(prod_ind,:),0,0,0);
         den = mm.r+abs(diag(mm.Q_h));
-        perm_flow = -mm.cost_h(squeeze(l_opt(1,k,mm.net_size+1,j,:)),mm.net_size+1)+mm.theta1(k)*squeeze(l_opt(1,k,mm.net_size+1,j,:)).*policy.pi_h(j,:)';
+        perm_flow = -mm.cost_h(squeeze(l_opt(1,k,mm.net_size+1,prod_ind,:)),mm.net_size+1)+mm.theta1(k)*squeeze(l_opt(1,k,mm.net_size+1,prod_ind,:)).*policy.pi_h(prod_ind,:)';
         Q_diag = -mm.Q_h_d;
         Q_diag(1:size(mm.Q_h_d,1)+1:end) = den;
-        V(1,k,mm.net_size+1,j,:) = max(Q_diag^-1*perm_flow,0);
+        V(1,k,mm.net_size+1,prod_ind,:) = max(Q_diag^-1*perm_flow,0);
 
         for N = 1:mm.net_size
 
-            init = squeeze(V(1,k,mm.net_size-N+2,j,:));
-            new_v = val_func_iter(init,mm.b,mm.theta1(k),policy.pi_h(j,:)',mm.net_size+1-N,mm.Q_h,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,j,:)),1,mm.gam,1,mm.cs_h,mm.v_tolerance,mm.v_rel_tolerance,1,[],[],[],mm.cost_h,mm.l_opt_func_h,policy.pi_h(j,:)');
-            V(1,k,mm.net_size-N+1,j,:) = new_v;
-            [~,~,l_opt(1,k,mm.net_size+1-N,j,:)] = iterateValueFunction_h(squeeze(V(1,k,mm.net_size+1-N,j,:)),mm.theta1(k),policy.pi_h(j,:)',mm.net_size+1-N,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,j,:)),mm.cost_h,mm.l_opt_func_h);
+            val_guess = squeeze(V(1,k,mm.net_size-N+2,prod_ind,:));
+            new_v = val_func_iter(val_guess,mm.b,mm.theta1(k),policy.pi_h(prod_ind,:)',mm.net_size+1-N,mm.Q_h,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,prod_ind,:)),1,mm.gam,1,mm.cs_h,mm.v_tolerance,mm.v_rel_tolerance,1,[],[],[],mm.cost_h,mm.l_opt_func_h,policy.pi_h(prod_ind,:)');
+            V(1,k,mm.net_size-N+1,prod_ind,:) = new_v;
+            [~,~,l_opt(1,k,mm.net_size+1-N,prod_ind,:)] = iterateValueFunction_h(squeeze(V(1,k,mm.net_size+1-N,prod_ind,:)),mm.theta1(k),policy.pi_h(prod_ind,:)',mm.net_size+1-N,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,prod_ind,:)),mm.cost_h,mm.l_opt_func_h);
 
         end
     end
--- val_func_iter.m
+++ val_func_iter.m
@@ -9,18 +9,26 @@ function [new_v] = val_func_iter(init,bet,a,pi,net,Q0,rh,diag_Q,Q0_d,V_succ,V_fa
      max_pi = max(pi_orig);
 
      while differ > tol/scl && differ_rel > rel_tol/scl && iter < max_iter
-                  
+
          if no_learning == 1
-            new_v = old_v + iterateValueFunction_h(old_v,a,pi,net,rh,diag_Q,Q0_d,V_succ,c,l_opt_func) + old_v;
+            new_v = old_v + iterateValueFunction_h(old_v,a,pi,net,rh,diag_Q,Q0_d,V_succ,c,l_opt_func);
          else
             new_v = old_v + iterateValueFunction_f(old_v,a,pi,N+1-m,j,rh,diag_Q,Q0_d,V_fail,V_succ,c,l_opt_func);
          end
          
+         if isnan(new_v)
+             return;
+         end
+
          differ = sum(sum((new_v - old_v).*(new_v - old_v))); 
          differ_rel = differ / max_pi;
          damp = min(max_damp,1 - 1/iter);
          old_v = damp * old_v + (1 - damp) * new_v;
          iter = iter + 1;
+
+         if isnan(old_v)
+             return;
+         end
      end
 
 end

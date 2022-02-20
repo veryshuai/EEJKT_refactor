ab338656bf413ad6303de2d66f12045dcff85cd8
veryshuai <dj.eco@cbs.dk>
2022-02-20 13:12:35 +0100

    now the foreign value function solves as well

--- val_func_iter.m
+++ val_func_iter.m
@@ -1,20 +1,19 @@
-function [new_v,nostop,differ] = val_func_iter(init,bet,a,pi,net,Q0,rh,diag_Q,Q0_d,V_succ,V_fail,gam,scl,cscale,tol,rel_tol,no_learning,N,m,j,c,l_opt_func,pi_orig)
+function [new_v] = val_func_iter(init,bet,a,pi,net,Q0,rh,diag_Q,Q0_d,V_succ,V_fail,gam,scl,cscale,tol,rel_tol,no_learning,N,m,j,c,l_opt_func,pi_orig)
 
      differ = 2;
      differ_rel = 2;
      old_v = init;
      max_damp = 0;
      iter = 0;
-     nostop = 0;
      max_iter = 5e4;
      max_pi = max(pi_orig);
 
      while differ > tol/scl && differ_rel > rel_tol/scl && iter < max_iter
                   
          if no_learning == 1
-            new_v = iterateValueFunction_h(old_v,a,pi,net,rh,diag_Q,Q0_d,V_succ,c,l_opt_func) + old_v;
+            new_v = old_v + iterateValueFunction_h(old_v,a,pi,net,rh,diag_Q,Q0_d,V_succ,c,l_opt_func) + old_v;
          else
-            new_v = iterateValueFunction_f(old_v,bet,a,pi,N+1-m,j,size(Q0,1),rh,diag_Q,Q0_d,V_fail,V_succ,gam,scl,cscale,c,l_opt_func) + old_v;
+            new_v = old_v + iterateValueFunction_f(old_v,a,pi,N+1-m,j,rh,diag_Q,Q0_d,V_fail,V_succ,c,l_opt_func);
          end
          
          differ = sum(sum((new_v - old_v).*(new_v - old_v))); 

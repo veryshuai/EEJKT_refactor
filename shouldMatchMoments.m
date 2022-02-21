897be1206a90bfd8e1bb1c95e21fbdd3ecea7dd9
veryshuai <dj.eco@cbs.dk>
2022-02-21 10:03:33 +0100

    changed variable names in acceptance test function to be more clear

--- shouldMatchMoments.m
+++ shouldMatchMoments.m
@@ -1,14 +1,14 @@
-function shouldMatchMoments(new_moments,new_fit,write_output,filepath)
+function shouldMatchMoments(new_moment1,new_moment2,write_output,filepath)
 
     if write_output == "overwrite"
-        old_moments = new_moments;
-        old_fit = new_fit;
-        save(filepath,'old_moments','old_fit')
+        old_moment1 = new_moment1;
+        old_moment2 = new_moment2;
+        save(filepath,'old_moment1','old_moment2')
     elseif write_output == "test"
         load(filepath);
-        match_all_moments = min(new_moments == old_moments,[],'all');
-        match_fit = min(new_fit == old_fit,[],'all');
-        assert(min(match_all_moments,match_fit)==1,"shouldMatchMoments: Benchmark moments do not match test moments");
+        match_moment1 = min(new_moment1 == old_moment1,[],'all');
+        match_moment2 = min(new_moment2 == old_moment2,[],'all');
+        assert(min(match_moment1,match_moment2)==1,"shouldMatchMoments: Benchmark moments do not match test moments");
     else
         error("shouldMatchOutput: Third argument must be either overwrite or test.")
     end

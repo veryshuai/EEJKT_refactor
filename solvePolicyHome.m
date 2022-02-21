5606e7439c658309513487987c6a6bebcf8437e7
veryshuai <dj.eco@cbs.dk>
2022-02-21 12:54:39 +0100

    have now updated the home value function script, and results match.

--- solvePolicyHome.m
+++ solvePolicyHome.m
@@ -1,27 +1,24 @@
 function [V,l_opt] = solvePolicyHome(policy,mm)
 
-V = zeros(1,mm.dim1,mm.net_size+1,2*mm.phi_size+1,2*mm.x_size+1);
-%lambda_h (common succ rate (defunct), known succ rate, network size, prod of firm, H macro shock)
-l_opt = zeros(size(V)); 
+V = zeros(size(mm.Q_px_h,1),1,mm.dim1,mm.net_size+1); %[macro-prod states, theta_g,theta_h, max network effect]
+l_opt = zeros(size(V));
 
-for prod_ind = 1:size(mm.Phi)
-    for k = 1:mm.dim1
-        
-        l_opt(1,k,mm.net_size+1,prod_ind,:) = mm.l_opt_func_h(mm.theta1(k),mm.net_size,policy.pi_h(prod_ind,:),0,0,0);
-        den = mm.r+abs(diag(mm.Q_h));
-        perm_flow = -mm.cost_h(squeeze(l_opt(1,k,mm.net_size+1,prod_ind,:)),mm.net_size+1)+mm.theta1(k)*squeeze(l_opt(1,k,mm.net_size+1,prod_ind,:)).*policy.pi_h(prod_ind,:)';
-        Q_diag = -mm.Q_h_d;
-        Q_diag(1:size(mm.Q_h_d,1)+1:end) = den;
-        V(1,k,mm.net_size+1,prod_ind,:) = max(Q_diag^-1*perm_flow,0);
+for k = 1:mm.dim1
 
-        for N = 1:mm.net_size
+    l_opt(:,1,k,mm.net_size+1) = mm.l_opt_func_h(mm.theta1(k),mm.net_size,policy.pi_h,0,0,0);
+    den = mm.r+abs(diag(mm.Q_px_h));
+    perm_flow = -mm.cost_h(l_opt(:,1,k,mm.net_size+1),mm.net_size+1)+mm.theta1(k)*l_opt(:,1,k,mm.net_size+1).*policy.pi_h;
+    Q_diag = -mm.Q_px_h_d;
+    Q_diag(1:size(mm.Q_px_h_d,1)+1:end) = den;
+    V(:,1,k,mm.net_size+1) = max(Q_diag^-1*perm_flow,0);
 
-            val_guess = squeeze(V(1,k,mm.net_size-N+2,prod_ind,:));
-            new_v = val_func_iter(val_guess,mm.b,mm.theta1(k),policy.pi_h(prod_ind,:)',mm.net_size+1-N,mm.Q_h,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,prod_ind,:)),1,mm.gam,1,mm.cs_h,mm.v_tolerance,mm.v_rel_tolerance,1,[],[],[],mm.cost_h,mm.l_opt_func_h,policy.pi_h(prod_ind,:)');
-            V(1,k,mm.net_size-N+1,prod_ind,:) = new_v;
-            [~,~,l_opt(1,k,mm.net_size+1-N,prod_ind,:)] = iterateValueFunction_h(squeeze(V(1,k,mm.net_size+1-N,prod_ind,:)),mm.theta1(k),policy.pi_h(prod_ind,:)',mm.net_size+1-N,mm.r,abs(diag(mm.Q_h)),mm.Q_h_d,squeeze(V(1,k,mm.net_size-N+2,prod_ind,:)),mm.cost_h,mm.l_opt_func_h);
+    for N = 1:mm.net_size
 
-        end
+        val_guess = V(:,1,k,mm.net_size-N+2);
+        new_v = val_func_iter(val_guess,mm.b,mm.theta1(k),policy.pi_h,mm.net_size+1-N,mm.Q_px_h,mm.r,abs(diag(mm.Q_px_h)),mm.Q_px_h_d,V(:,1,k,mm.net_size-N+2),1,mm.gam,1,mm.cs_h,mm.v_tolerance,mm.v_rel_tolerance,1,[],[],[],mm.cost_h,mm.l_opt_func_h,policy.pi_h);
+        V(:,1,k,mm.net_size-N+1) = new_v;
+        [~,~,l_opt(:,1,k,mm.net_size+1-N)] = iterateValueFunction_h(V(:,1,k,mm.net_size+1-N),mm.theta1(k),policy.pi_h,mm.net_size+1-N,mm.r,abs(diag(mm.Q_px_h)),mm.Q_px_h_d,V(:,1,k,mm.net_size-N+2),mm.cost_h,mm.l_opt_func_h);
+
     end
 end
 end
--- solve_v1.m
+++ solve_v1.m
@@ -2,18 +2,19 @@ function policy = solve_v1(mm)
 
     policy = struct();
 
-    [policy.pi_f,policy.c_val_f] = solveExpectedMatchProfit(mm.scale_f,mm.px_f,mm.Q_px_f,mm.Q_px_f_d,mm.F_f,mm); %pi[prod,macro]
-    [policy.pi_h,policy.c_val_h] = solveExpectedMatchProfit(mm.scale_h,mm.px_h,mm.Q_px_h,mm.Q_px_h_d,mm.F_h,mm); %c_val[demand shk, prod,macro]
-    
-    shouldMatchMoments(policy.pi_f,policy.c_val_f,"test","results/shouldMatchProfitsForeignData");
-    shouldMatchMoments(policy.pi_h,policy.c_val_h,"test","results/shouldMatchProfitsHomeData");
+    [policy.pi_f,policy.c_val_f] = solveExpectedMatchProfit(mm.scale_f,mm.px_f,mm.Q_px_f,mm.Q_px_f_d,mm.F_f,mm); 
+    [policy.pi_h,policy.c_val_h] = solveExpectedMatchProfit(mm.scale_h,mm.px_h,mm.Q_px_h,mm.Q_px_h_d,mm.F_h,mm); 
 
     policy.postSuccessProb_f = makeForeignSuccessPosteriors(mm); %[trials,successes]
 
     [policy.value_h,policy.lambda_h] = solvePolicyHome(policy,mm);  
     [policy.value_f,policy.lambda_f] = solvePolicyForeign(policy,mm); 
 
+    shouldMatchMoments(policy.pi_f,policy.c_val_f,"test","results/shouldMatchProfitsForeignData");
+    shouldMatchMoments(policy.pi_h,policy.c_val_h,"test","results/shouldMatchProfitsHomeData");
     shouldMatchMoments(val_f,lf,"test","results/shouldMatchPolicyForeignData");
     shouldMatchMoments(val_h,lh,"test","results/shouldMatchPolicyHomeData");
 
+    %pi[prod,macro]
+    %c_val[demand shk, prod,macro]
 end

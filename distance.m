3ff83b8528c51f286738c5272c339e6594302d85
veryshuai <dj.eco@cbs.dk>
2022-03-08 15:40:33 +0100

    deleted some spaces form distance

--- distance.m
+++ distance.m
@@ -5,10 +5,10 @@ X = [-3.60529  -3.87941  0.23156  2.46487  1.88176  15.42634 0.38246  11.72211  
 
 rng(80085,'twister');
 seed_crand(80085);
-    
+
 mm = setModelParameters(X);
 policy = generatePolicyAndValueFunctions(mm);
 simMoms = simulateMomentsMain(policy,mm);
 calculateDistanceAndPrint(simMoms,mm,X);
- 
+
 end

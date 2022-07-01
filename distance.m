97d3fe0f9e31a0582edd9b53e4a752e1a46260c8
Jim Tybout <jxt32@psu.edu>
2022-07-01 09:18:42 -0400

    distance update

--- distance.m
+++ distance.m
@@ -4,6 +4,8 @@ format long;
 % X = [-3.60529  -3.87941  0.23156  2.46487  1.88176  15.42634 0.38246  11.72211  1.38618  -1.21819  13.00238  -6.13506];
 % X = [-3.60529  -3.87941  0.23156  2.46487  1.88176  15.42634 0.38246  11.72211  1.88618  -1.21819  13.00238  -6.13506];
 
+try
+
 rng(80085,'twister');
 seed_crand(80085);
 
@@ -11,5 +13,9 @@ mm = setModelParameters(X);
 policy = generatePolicyAndValueFunctions(mm);
 simMoms = simulateMomentsMain(policy,mm);
 [D,~] = calculateDistanceAndPrint(simMoms,mm,X);
+
+catch
+    D = NaN;
+end
 
 end  

caf9b035f0c253ce909f883eadfb94f07b6c1b73
veryshuai <dj.eco@cbs.dk>
2022-07-14 15:15:40 +0200

    found a bug in the new code, now new value and lambda functions are close to the old ones

--- solvePolicyForeign.m
+++ solvePolicyForeign.m
@@ -7,20 +7,20 @@ l_opt = zeros(size(V));
 for prod_ind = 1:size(mm.Phi)
     for succ = 1:mm.n_size+1
     
-        l_opt(mm.n_size+1,succ,1,mm.net_size+1,prod_ind,:) = mm.l_opt_func_f(policy.postSuccessProb_f(mm.n_size+1,succ),mm.net_size+1,policy.pi_f(prod_ind,:),0,0,0);
+        l_opt(succ,mm.n_size+1,1,mm.net_size+1,prod_ind,:) = mm.l_opt_func_f(policy.postSuccessProb_f(mm.n_size+1,succ),mm.net_size+1,policy.pi_f(prod_ind,:),0,0,0);
         Q0_diag = -mm.Q_f_d;
         Q0_diag(1:size(mm.Q_f_d,1)+1:end) = mm.r+abs(diag(mm.Q_f));
-        perm_flow = policy.postSuccessProb_f(mm.n_size+1,succ)*squeeze(l_opt(mm.n_size+1,succ,1,mm.net_size+1,prod_ind,:)).*policy.pi_f(prod_ind,:)'-mm.cost_f(squeeze(l_opt(mm.n_size+1,succ,1,mm.net_size+1,prod_ind,:)),mm.net_size+1);
+        perm_flow = policy.postSuccessProb_f(mm.n_size+1,succ)*squeeze(l_opt(succ,mm.n_size+1,1,mm.net_size+1,prod_ind,:)).*policy.pi_f(prod_ind,:)'-mm.cost_f(squeeze(l_opt(succ,mm.n_size+1,1,mm.net_size+1,prod_ind,:)),mm.net_size+1);
         % next eq. follows from V = (mm.r + abs(diag(Q0)))^-1 * (perm_flow + Q0_d * V)
-        V(mm.n_size+1,succ,1,mm.net_size+1,prod_ind,:) = max(Q0_diag^-1*perm_flow,0);
+        V(succ,mm.n_size+1,1,mm.net_size+1,prod_ind,:) = max(Q0_diag^-1*perm_flow,0);
     
         % learning maxed out
         for m = 1:mm.net_size+1-succ
     
-            new_v = val_func_iter('no_learning',squeeze(V(mm.n_size+1,succ,1,mm.net_size+2-m,prod_ind,:)),squeeze(V(mm.n_size+1,succ,1,mm.net_size+2-m,prod_ind,:)),[],policy.postSuccessProb_f(mm.n_size+1,succ),policy.pi_f(prod_ind,:),mm.net_size+1-m,mm,policy,'foreign');
-            V(mm.n_size+1,succ,1,mm.net_size+1-m,prod_ind,:) = new_v;
-            [~,l_opt(mm.n_size+1,succ,1,mm.net_size+1-m,prod_ind,:)] = iterationError_h(squeeze(V(mm.n_size+1,succ,1,mm.net_size+1-m,prod_ind,:)),...
-                policy.postSuccessProb_f(mm.n_size+1,succ),policy.pi_f(prod_ind,:),mm.net_size+1-m,squeeze(V(mm.n_size+1,succ,1,mm.net_size+2-m,prod_ind,:)),mm,policy,'foreign');
+            new_v = val_func_iter('no_learning',squeeze(V(succ,mm.n_size+1,1,mm.net_size+2-m,prod_ind,:)),squeeze(V(succ,mm.n_size+1,1,mm.net_size+2-m,prod_ind,:)),[],policy.postSuccessProb_f(mm.n_size+1,succ),policy.pi_f(prod_ind,:),mm.net_size+1-m,mm,policy,'foreign');
+            V(succ,mm.n_size+1,1,mm.net_size+1-m,prod_ind,:) = new_v;
+            [~,l_opt(succ,mm.n_size+1,1,mm.net_size+1-m,prod_ind,:)] = iterationError_h(squeeze(V(succ,mm.n_size+1,1,mm.net_size+1-m,prod_ind,:)),...
+                policy.postSuccessProb_f(mm.n_size+1,succ),policy.pi_f(prod_ind,:),mm.net_size+1-m,squeeze(V(succ,mm.n_size+1,1,mm.net_size+2-m,prod_ind,:)),mm,policy,'foreign');
     
         end
     end
@@ -28,9 +28,9 @@ for prod_ind = 1:size(mm.Phi)
     for tri = 1:mm.n_size
         for succ = 1:mm.n_size+1-tri
     
-            new_v = val_func_iter('learning',squeeze(V(mm.n_size+2-tri,succ,1,succ,prod_ind,:)),squeeze(V(mm.n_size-tri+2,succ+1,1,succ+1,prod_ind,:)),squeeze(V(mm.n_size-tri+2,succ,1,succ,prod_ind,:)),policy.postSuccessProb_f(mm.n_size+1-tri,succ),policy.pi_f(prod_ind,:)',succ,mm,policy,'foreign');
-            V(mm.n_size+1-tri,succ,1,succ,prod_ind,:) = new_v;
-            [~,l_opt(mm.n_size+1-tri,succ,1,succ,prod_ind,:)] = iterationError_f(squeeze(V(mm.n_size+1-tri,succ,1,succ,prod_ind,:)),policy.postSuccessProb_f(mm.n_size+1-tri,succ),policy.pi_f(prod_ind,:)',succ,squeeze(V(mm.n_size-tri+2,succ,1,succ,prod_ind,:)),squeeze(V(mm.n_size-tri+2,succ+1,1,succ+1,prod_ind,:)),mm);
+            new_v = val_func_iter('learning',squeeze(V(succ,mm.n_size+2-tri,1,succ,prod_ind,:)),squeeze(V(succ+1,mm.n_size-tri+2,1,succ+1,prod_ind,:)),squeeze(V(succ,mm.n_size-tri+2,1,succ,prod_ind,:)),policy.postSuccessProb_f(mm.n_size+1-tri,succ),policy.pi_f(prod_ind,:)',succ,mm,policy,'foreign');
+            V(succ,mm.n_size+1-tri,1,succ,prod_ind,:) = new_v;
+            [~,l_opt(succ,mm.n_size+1-tri,1,succ,prod_ind,:)] = iterationError_f(squeeze(V(succ,mm.n_size+1-tri,1,succ,prod_ind,:)),policy.postSuccessProb_f(mm.n_size+1-tri,succ),policy.pi_f(prod_ind,:)',succ,squeeze(V(succ,mm.n_size-tri+2,1,succ,prod_ind,:)),squeeze(V(succ+1,mm.n_size-tri+2,1,succ+1,prod_ind,:)),mm);
 
         end
     end

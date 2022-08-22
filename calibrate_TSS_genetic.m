0df7da5e88c5027ef2879803c8426adbfd7305a2
Jim Tybout <jxt32@psu.edu>
2022-08-22 10:13:04 -0400

    fixed error in fprint statement

---
+++ calibrate_TSS_genetic.m
@@ -0,0 +1,101 @@
+%This function initiates the genetic algorithm to solve for
+%the parameters of the model.  Initial parameter guesses can be set here,
+%as well as optional settings for the genetic algorithm.
+
+
+% relation between parameters and the X vector:
+
+%% relation between parameters and the X vector:
+    
+% F_h      =  exp(X(1));     % home match fixed cost
+% scale_h  =  X(2);          % log of home profit function scalar       
+% delta    =  0.326;         % exogenous match death hazard (per year)                                 
+% beta     = 1;              % cost function convexity parameter               
+% ah       =  X(4)*X(3);     % X(3) is mean of beta dist ah / (ah + bh)
+% bh       =  X(4)*(1-X(3)); % X(4) is (ah + bh)        
+% L_z      =  4;             % buyer shock jump hazard (once per quarter)           
+% D_z      =  X(5);          % buyer shock jump size            
+% L_b      =  X(6);          % shipment hazard            
+% gam      =  X(7);          % network effect
+% cs_h     =  exp(X(8));     % cost function scalar, home market  
+% sig_p    =  X(9);          % std. dev. of log productivity shock 
+% F_f      =  exp(X(10));    % foreign match fixed cost
+% cs_f     =  exp(X(11));    % cost function scalar, foreign market
+% scale_f  =  X(12);         % log of foreign profit function scalar 
+
+%%
+      
+  theta = [-6.35647  -5.27227   0.21626   1.56615   1.18474  13.08123...
+            0.25626   3.64325   1.46860  -0.43414   9.10446  -9.30786];      
+  D = length(theta);
+        
+% population size
+PS = 2*D;
+disp(['Population size ' num2str(PS)])
+H = rand(PS,D);
+
+bounds = [theta - 0.50*abs(theta); theta + 0.50*abs(theta)]';
+
+lb = bounds(:,1);
+ub = bounds(:,2);
+IR = bounds';
+lb = lb(:,ones(PS,1))';
+ub = ub(:,ones(PS,1))';
+rng = abs(ub-lb);
+X0 = lb + rng.*H;
+clear lb ub rng
+% initial population
+population = X0;
+
+cf = 0.30;          % crossover fraction
+EC = 4;             % elite count
+shrink = 1;
+SF = 5; 
+
+GN = 50; % number of generations per run
+R =  50; % total number of runs
+
+scale0 = 0.15;
+t = 1;
+flag = true;
+while t<=R
+    scale = ((R-t)/R)*scale0;
+    mut_fn = @(parents,options,GenomeLength,FitnessFcn,state,thisScore,...
+        thisPopulation) ...
+        TSS_mutationgaussian(parents,options,GenomeLength,FitnessFcn,...
+        state,thisScore,thisPopulation,scale,shrink,t,R,GN);
+    disp(['optimization run ',num2str(t),' of ',num2str(R)])
+    options = gaoptimset('TolFun',1e-20,'StallGenLimit',800,'MutationFcn',mut_fn,...
+        'Generations',GN,'PopulationSize',PS,'InitialPopulation',population,'Display','iter',...
+        'UseParallel','always','Vectorized', 'off','CrossoverFraction',cf,'EliteCount',EC,...
+        'PopInitRange',IR,'FitnessScaling',@fitscalingrank);
+    disp('optimization starting')
+    tic
+    [x,fval_ga,exitflag,output,population] = ga(@(X) distance(X),D,[],[],[],[],...
+        [],[],[],options);
+    FileName = ['Output/' 'Optimization','_',datestr(now,'yyyy_mmdd_HHMM'),...
+        '_Run',num2str(t),'of',num2str(R)];
+    save(FileName,'x','fval_ga','population')
+    time_elapsed1 = toc;
+    disp(['time elapsed in total: '...
+    num2str(time_elapsed1/60) ...
+    ' minutes'])
+    disp(['time elapsed per generation: ' ...
+    num2str(time_elapsed1/(GN+1)) ...
+    ' seconds'])
+   pop_range_t = [min(population',[],2) max(population',[],2)];
+   IRt = IR';
+   pop_range0 = round(100*mean(abs(pop_range_t(:,2)-pop_range_t(:,1))./abs(IRt(:,2)-IRt(:,1))));
+   disp(['Average (max minus min) population range as percentage of initial range: ' num2str(pop_range0)])
+   pop_range1 = round(100*(sum(pop_range_t(:,1)<IRt(:,1)) + sum(pop_range_t(:,2)>IRt(:,2)))/(2*D));
+   disp(['Percentage of initial bounds that are violated by at least one parameter vector in the current population: ' ...
+       num2str(pop_range1)])
+   
+    t = t + 1;
+end
+theta = x';
+
+
+
+
+

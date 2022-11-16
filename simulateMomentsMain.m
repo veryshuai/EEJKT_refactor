8cc2b5301cca32a21bc3f2bec1eaa3b36095ce1c
Jim Tybout <jxt32@psu.edu>
2022-11-16 15:37:31 -0500

    small stuff--preparing to pull

--- simulateMomentsMain.m
+++ simulateMomentsMain.m
@@ -33,11 +33,11 @@ end
 
 %% Uncomment commands below to generate data for spot checks
 
-check_cell_H = sim_out{mm.check_type}.iterH_check;
-check_cell_F = sim_out{mm.check_type}.iterF_check;
-check_count_H = sim_out{mm.check_type}.stackH;
-check_count_F = sim_out{mm.check_type}.stackF;
-save 'iter_out_checks.mat' 'check_type' 'check_cell_H' 'check_cell_F' 'check_count_H' 'check_count_F';  
+% check_cell_H = sim_out{mm.check_type}.iterH_check;
+% check_cell_F = sim_out{mm.check_type}.iterF_check;
+% check_count_H = sim_out{mm.check_type}.stackH;
+% check_count_F = sim_out{mm.check_type}.stackF;
+% save 'iter_out_checks.mat' 'check_type' 'check_cell_H' 'check_cell_F' 'check_count_H' 'check_count_F';  
 
 sim_cum = aggregateSimulatedData(sim_out,mm);
 

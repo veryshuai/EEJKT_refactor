dde89fcdca9c12d46a967b07b8f803067c622679
veryshuai <dj.eco@cbs.dk>
2023-01-03 18:29:31 +0100

    made a few more corrections to the new way of making the transition matrices and doing firm death

--- simulateForeignMatchesInnerSimUpdateClientCount.m
+++ simulateForeignMatchesInnerSimUpdateClientCount.m
@@ -13,20 +13,28 @@ function [iter_in, drop_Zcut] = simulateForeignMatchesInnerSimUpdateClientCount(
     iter_in.add_cli_cnt(:,iter_in.t) = max(iter_in.cum_succ(:,iter_in.t) - iter_in.cum_succ(:,iter_in.t-1),0); 
     %max() resets count to 0 for neg. differences to deal with new exporter
 %    iter_in.new_firm(:,iter_in.t)    = max((iter_in.cum_meets(:,iter_in.t) - iter_in.cum_meets(:,iter_in.t-1) < 0),(stay == 0)); 
-    iter_in.new_firm(:,iter_in.t)    = stay == 0; 
 % first period for new exporter
 %   iter_in.exit_firm(:,iter_in.t-1) = iter_in.cum_meets(:,iter_in.t) - iter_in.cum_meets(:,iter_in.t-1) < 0 ; 
-    iter_in.exit_firm(:,iter_in.t-1) = stay == 0 & iter_in.cum_meets(:,iter_in.t-1) ~= 0;  
     % = 1 if t-1 is last period before exit
     %DJ: This way of doing exit firm should preserve the idea that a firm
-    %only exits if it has positive meetings.
+    %only exits if it has ever had positive meetings.
     [iter_in, drop_Zcut, drop_cnt] = simulateForeignMatchesInnerSimDrops(iter_in, policy, mm);
     iter_in.cur_cli_cnt(:,iter_in.t) = iter_in.add_cli_cnt(:,iter_in.t) + iter_in.cur_cli_cnt(:,iter_in.t-1) ...
         - drop_cnt - iter_in.exog_deaths(:,iter_in.t-1) ;
     
-    iter_in.cur_cli_cnt(:,iter_in.t) =...
-        (1 - iter_in.new_firm(:,iter_in.t).*(1-iter_in.new_firm(:,iter_in.t-1)))...
-        .*iter_in.cur_cli_cnt(:,iter_in.t) ; % to reset client counts when exits occur 
+    % iter_in.cur_cli_cnt(:,iter_in.t) =...
+    %    (1-iter_in.new_firm(:,iter_in.t-1)))...
+    %    .*iter_in.cur_cli_cnt(:,iter_in.t) ; % to reset client counts when exits occur 
+
+    %if death, reset everything (should already be correct for non
+    %learners, but learners need to be set to zero)
+    firm_died = ~stay;
+    iter_in.new_firm(:,iter_in.t)    = firm_died; 
+    iter_in.exit_firm(:,iter_in.t-1) = firm_died & iter_in.cum_meets(:,iter_in.t-1) ~= 0;  
+    iter_in.cur_cli_cnt(firm_died,iter_in.t) = 0;
+    iter_in.cum_meets(firm_died,iter_in.t) = 0;
+    iter_in.cum_succ(firm_died,iter_in.t) = 0;
+
 end
 
 

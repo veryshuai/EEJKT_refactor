ec329a1dc1a8a4a5d0adb14bbfb0ac93bbeb22dc
Jim Tybout <jxt32@psu.edu>
2023-01-31 16:16:54 -0500

    changed match_shpt_cntr so that new firms are included in dud counts

--- match_shpt_cntr.m
+++ match_shpt_cntr.m
@@ -6,10 +6,27 @@ function [nship_obs,ln_ships,match_count,match_countD,dud_matches] = match_shpt_
 % match = mat_yr_sales: [firm ID, match-specific sales, shipments, boy Z, eoy Z, match age, firm age] 
 % match_count is a vector that counts the number of matches for each active firm
 
-yr_tlag  = iter_in.t-mm.pd_per_yr;
-matches  = iter_in.mat_yr_sales;
-duds     = iter_in.cur_duds(:,yr_tlag+1:iter_in.t);
-firm_age = iter_in.cumage(:,end);
+yr_tlag      = iter_in.t-mm.pd_per_yr;
+matches      = iter_in.mat_yr_sales;
+all_duds     = [(1:size(iter_in.cur_duds,1))',iter_in.cur_duds(:,yr_tlag+1:iter_in.t)];
+all_firm_age = iter_in.cumage(:,end);
+
+%% split firmID into multiple dud rows if hotel room turns over during the year
+
+% A firm is new until it makes a match. 
+[aa,bb] = find(iter_in.new_firm(:,yr_tlag+1:iter_in.t) - iter_in.new_firm(:,yr_tlag:iter_in.t-1)==-1);
+if ~isempty(aa)
+    newfirm_duds = zeros(length(aa),mm.pd_per_yr+1);
+    newfirm_age  = zeros(length(aa),1);
+    incumb_duds = all_duds;
+    for ii=1:length(aa)
+        newfirm_duds(ii,:) = [aa(ii)+0.5,zeros(1,bb(ii)-1),all_duds(aa(ii),bb(ii):mm.pd_per_yr)];
+        incumb_duds(aa(ii),:) = [incumb_duds(aa(ii),1:bb(ii)),zeros(1,mm.pd_per_yr - bb(ii)+1)]; 
+        newfirm_age(ii) = mm.pd_per_yr - bb(ii); 
+    end
+    all_duds = [incumb_duds;newfirm_duds];
+    all_firm_age = [all_firm_age;newfirm_age];
+end
 
 %% find number of shipments for each match, take logs and sum
  % firms with integer IDs & shipments>0:
@@ -36,9 +53,7 @@ match_count = double.empty(0,1);
      
 %% stack dud matches with others to get alternative match count
 
-% Note: this dud count will miss duds at new firms during their first year
-
-dud_count   = [(1:size(duds,1))',sum(duds,2),firm_age];
+dud_count   = [all_duds(:,1),sum(all_duds(:,2:end),2),all_firm_age];
 
 dud_matches = double.empty(0,7);
 
@@ -48,7 +63,7 @@ macro_shk = mm.X_f(iter_in.macro_state_f(iter_in.t));
 dud_rev   = exp(scale + (mm.eta-1)*mm.Phi(mm.pt_type(iter_in.pt_ndx,1)) + macro_shk).*exp(iter_in.Zcut_eoy); 
 
 % create records of dud matches
-for ii = 1:size(duds,1)
+for ii = 1:size(all_duds,1)
   Ndud = dud_count(ii,2);
   if Ndud >0  
   % dud matches: firm_ID, shipment=1, sale=dud_rev, bop Z = eop Z = match_age = 0, and actual firm age     
@@ -70,12 +85,14 @@ ff2D_obs = find((floor(matchesD(:,1))-matchesD(:,1)~=0).*(matchesD(:,3) >0));
 % number of matches w/ shipments > 0
       
     if ~isempty(ff1D_obs) % incumbent firm match counts       
-        incumb_matchD = sum(dummyvar(matchesD(ff1D_obs,1)))';     
+        % Note: had to double match ID bec. dummyvar only works with integers
+        % All incumbents become even firmIDs, all new firms become odd IDs
+        incumb_matchD = sum(dummyvar(2*matchesD(ff1D_obs,1)))';     
         match_countD = sortrows(incumb_matchD(incumb_matchD>0));
      end
      
      if~isempty(ff2D_obs) % new firm counts stacked with incumbent counts
-        newfirm_matchD = sum(dummyvar(matchesD(ff2D_obs,1)))';   
+        newfirm_matchD = sum(dummyvar(2*matchesD(ff2D_obs,1)))';   
         match_countD = [match_countD; sortrows(newfirm_matchD(newfirm_matchD>0))];
         match_countD = sortrows(match_countD);
      end

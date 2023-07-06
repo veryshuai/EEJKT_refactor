6b742ead867966305155d862facc3046c8665a16
Jim Tybout <jxt32@psu.edu>
2023-07-06 15:36:30 -0400

    slight clean-up of degree_dist

--- degree_dist.m
+++ degree_dist.m
@@ -1,6 +1,6 @@
 function FirmCount = degree_dist(match_list,mm)
 
-   maxYear         = max(match_list(:,1));        
+   maxYear     = max(match_list(:,1));        
    match_TF    = match_list(:,2) + 0.0001*match_list(:,3); % firm type by firmID identifier
    TF_list     = unique(match_TF);
    NumTF       = size(TF_list,1);    
@@ -9,9 +9,9 @@ function FirmCount = degree_dist(match_list,mm)
    firmMatchCount = zeros(NumTF,maxYear);
    for TF_id = 1:NumTF 
      ff = match_TF == ones(size(match_list,1),1).*TF_list(TF_id);
-     firmYr = match_list(ff,:); 
+     firmRecs = match_list(ff,:); 
        for yr=1:maxYear
-        firmMatchCount(TF_id,yr) = sum(firmYr(:,1)==yr);
+        firmMatchCount(TF_id,yr) = sum(firmRecs(:,1)==yr);
        end    
    end
 
@@ -19,9 +19,9 @@ function FirmCount = degree_dist(match_list,mm)
  % convert to frequency counts of # firms with each possible match count
    MatFreq = zeros(maxMatch,maxYear);  
    for yr=1:maxYear
-   if sum(firmMatchCount(:,yr))>0 % counts excluding duds
+     if sum(firmMatchCount(:,yr))>0 % counts excluding duds
      MatFreq(:,yr) = sum(firmMatchCount(:,yr)*ones(1,mm.max_match) - ones(size(firmMatchCount(:,yr),1),1)*(1:mm.max_match)==0,1);
-   end
+     end
    end 
    
   FirmCount = sum(MatFreq(:,1:end),2); % including burn-in years

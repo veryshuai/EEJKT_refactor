e36ee466054c0b76c3d493df28f180cd9a85af4c
Jim Tybout <jxt32@psu.edu>
2023-11-07 10:25:18 -0500

    updated bootstrap.m and added bootstrap_logs.m

--- bootstrap_logs.m
+++ bootstrap_logs.m
@@ -18,7 +18,6 @@ X = [-3.84856595690155,-19.6507323165926,0.137095551436060,0.283174883410539,...
       0.574476212931893,12.0224286499768,0.0494381874453607,4.87592549089832,...
       2.38517796052932,15.1344721052573]; % fit: 11.8568 (clusters) 11.8619 (PC)
 
-
 mm = setModelParameters(X);
 mm.check_type = 108;
 
@@ -79,7 +78,8 @@ end
 
 fin_diff_size = (1 + 0.05); %percentage of parameter value 
 
-param_vec = [mm.F_h, mm.scale_h, mm.ah, mm.bh, mm.D_z, mm.L_bF, mm.gam, mm.cs_h, mm.sig_p, mm.cs_f]';
+% NOTE: expressing cost function scalars in logs before generating differences
+param_vec = [mm.F_h, mm.scale_h, mm.ah, mm.bh, mm.D_z, mm.L_bF, mm.gam, log(mm.cs_h), mm.sig_p, log(mm.cs_f)]';
 pv_siz    = size(param_vec,1);
 
 %each column is a finite differenced version of the parameter set
@@ -90,6 +90,7 @@ fin_diff_param_mat = [param_vec,fin_diff_param_mat]; %append "baseline" paramete
 % %generate a cell for each moment
 match_death_coefs_fd = cell(pv_siz + 1,1);% [match_exit_rate;beta_match_exit(2:5)]; % [match exit rate, 1st yr. dummy, lnXf(ijt), ln(match age), ln(exporter age),mse]
 match_ar1_coefs_fd   = cell(pv_siz + 1,1);% [ybar_match;beta_match(2:4);mse_match_ar1]; % [mean ln Xf(ijt), ln Xf(ijt-1), R(ijt-1), ln(exporter age)]
+%loglog_coefs_fd      = cell(pv_siz + 1,1);% [b_degree]; % [intercept, slope, quadratic term]
 mavship_fd           = cell(pv_siz + 1,1);% [avg_ln_ships]; % average ln(# shipments)
 exp_dom_coefs_fd     = cell(pv_siz + 1,1);% [ybar_hfsales;beta_hfsales(2);mse_hf]; % [mean dep var.,coef,MSE]
 dom_ar1_coefs_fd     = cell(pv_siz + 1,1);% [ybar_fsales_h;beta_fsales_h(2);mse_h]; % [mean dep var.,coef,MSE]
@@ -98,15 +99,22 @@ succ_rate_coefs_fd   = cell(pv_siz + 1,1);% [mean_succ_rate;b_succ_rate(2)]; % [
 sr_var_coefs_fd      = cell(pv_siz + 1,1);% [mean_usq_succ;b_usq_succ(2)]; % [mean dep. var, ln(1+meetings)]
 for_sales_shr_fd     = cell(pv_siz + 1,1);% [avg_expt_rate]; % mean share of exports to U.S. in total sales
 exp_frac_fd          = cell(pv_siz + 1,1);% [share_exptr]; % fraction of firms exporting to U.S.
+spb_fd               = cell(pv_siz + 1,1);% [simMoms.model_shareD]; % fraction of firms with 1 buyer, 2 buyers, etc.
 
 for iter =1:pv_siz + 1
 
     display(iter)
 
-    %point estimates
+  % point estimates
   %  mm = struct();
     cell_for_assignment = num2cell(fin_diff_param_mat(:,iter)); %need cell to unpack into multiple variables, matlab quirk
+
+  % undo log transformation for evaluation of parameter vector
+    cell_for_assignment{8} = exp(cell_for_assignment{8});    
+    cell_for_assignment{10} = exp(cell_for_assignment{10});  
+
     [mm.F_h, mm.scale_h, mm.ah, mm.bh, mm.D_z, mm.L_bF, mm.gam, mm.cs_h, mm.sig_p, mm.cs_f] = cell_for_assignment{:};
+    
 %   set constrained parameters equal to their perturbed counterpart values    
     mm.F_f = mm.F_h;
     mm.scale_f =  mm.scale_h;
@@ -136,7 +144,8 @@ end
 
 %Reset to baseline values
 cell_for_assignment = num2cell(fin_diff_param_mat(:,1)); %need cell to unpack into multiple variables
-[mm.F_h, mm.scale_h, mm.ah, mm.bh, mm.D_z, mm.L_bF, mm.gam, mm.cs_h, mm.sig_p, mm.cs_f] = cell_for_assignment{:};
+
+[mm.F_h, mm.scale_h, mm.ah, mm.bh, mm.D_z, mm.L_bF, mm.gam, mm.log_cs_h, mm.sig_p, mm.log_cs_f] = cell_for_assignment{:};
 
 
 % (Probably don't need this block since we're not constructing std. errors for these parameters)
@@ -147,8 +156,7 @@ cell_for_assignment = num2cell(fin_diff_param_mat(:,1)); %need cell to unpack in
 % (3) Construct moment covariance matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Form moment vector, need to have vector to construct full covarience
-%matrix
+%Form moment vector, need to have vector to construct full covarience matrix
 moments_boot_mat = zeros(size(Data,2),runs);
 for iter = 1:runs
     moments_boot_mat(:,iter) = [... 
@@ -206,7 +214,7 @@ AGS_elas = AGS_sens .* repmat((W * fin_diff_mat(:,1))',size(AGS_sens,1),1) ./ re
 % construct table for easy understanding of Gentzkow Shapiro weighting method
 
 % AGS_param_names = {'F', 'scale_h', 'ah', 'bh', 'D_z', 'L_bF', 'gam', 'cs_h', 'sig_p', 'cs_f', 'scale_f'};
-AGS_param_names = {'F', 'scale_h', 'ah', 'bh', 'D_z', 'L_bF', 'gam', 'cs_h', 'sig_p', 'cs_f'};
+AGS_param_names = {'F', 'log(Pi)', 'ah', 'bh', 'D_z', 'L_bF', 'gam', 'log(cs_h)', 'sig_p', 'log(cs_f)'};
 
 varnames = {'match_death_coefs1','match_death_coefs2','match_death_coefs3','match_death_coefs4','match_death_coefs5','match_ar1_coefs1','match_ar1_coefs2','match_ar1_coefs3','match_ar1_coefs4','match_ar1_coefs5','mavship','exp_dom_coefs1','dom_ar1_coefs2','match_lag_coefs1','match_lag_coefs2','succ_rate_coefs1','succ_rate_coefs2','sr_var_coefs1','sr_var_coefs2','for_sales_shr','exp_frac'};
 AGS_cell = mat2cell(AGS_elas,ones(pv_siz,1),ones(size(Data,2),1));
@@ -225,3 +233,4 @@ format short
 param_est = table(parameter,std_error,z_ratio,'Rownames',AGS_param_names)
 
 save results/bootstrap_results
+

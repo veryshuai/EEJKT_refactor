67916e8a06f171c7fc65504de6e8cdbb5128a7b1
veryshuai <dj.eco@cbs.dk>
2023-12-13 11:34:10 +0100

    Fixed memory issues with parallel loop.  The problem was that the policy cell was far too large to pass to each worker, something like 6GB.  Now we loop serially through the policy functions, and only do the parallel computing within each theta type.

--- simulateMomentsMain_nolearning.m
+++ simulateMomentsMain_nolearning.m
@@ -9,49 +9,54 @@ sim_out = cell(mm.N_pt,1);
 seeds = randi(1e6,size(mm.Phi,1),2);
 
 mm.start_time = tic;
-parfor pt_ndx = 1:mm.N_pt
-%for pt_ndx = 1:1:mm.N_pt 
-%for pt_ndx = 90
-% for pt_ndx = 106
-% for pt_ndx = 108
-% for pt_ndx = 101
-% for pt_ndx = 113
+for current_theta_ind = 1:mm.dim2
+    %We have to loop through the policy functions, as the policy cell is
+    %too large to pass to parallel
+    policy = policy_cell{current_theta_ind};
 
-    %Assign the relevant policy to this pt_ndx firm type
-    theta_ind = mm.pt_type(pt_ndx,2);
-    policy = policy_cell{theta_ind};
+    parfor pt_ndx = 1:mm.N_pt
+        %for pt_ndx = 1:1:mm.N_pt
+        %for pt_ndx = 90
+        % for pt_ndx = 106
+        % for pt_ndx = 108
+        % for pt_ndx = 101
+        % for pt_ndx = 113
 
-   if toc(mm.start_time) > mm.abort_time
-     display(toc(mm.start_time));
-     disp('simulateMomentsMain: Time limit reached in parameter evaluation');
-     err('Time limit reached')
-     fileID5 = fopen('results/EEJKT_maxtime_error.txt','a');
-     fprintf(fileID5,'\r\n  ');
-     fprintf(fileID5,'\r\n sim time exceeds %.0f in simulateMomentsMain', mm.abort_time);
-     fprintf(fileID5,'\r\n firm type = %.2f', pt_ndx);
-     fprintf(fileID5,'\r\n params = ');
-     fprintf(fileID5,'\r%8.5f %8.5f %8.5f %8.5f %8.5f %8.5f',mm.param_vec(1:6));
-     fprintf(fileID5,'\r%8.5f %8.5f %8.5f %8.5f %8.5f %8.5f',mm.param_vec(7:end));
-     fprintf(fileID5, '\r\n  ');  
-     fclose(fileID5);    
-   end
+        %Only run the code for pt_ndx with the current policy
+        theta_ind_ndx = mm.pt_type(pt_ndx,2);
+        if theta_ind_ndx == current_theta_ind
+            if toc(mm.start_time) > mm.abort_time
+                display(toc(mm.start_time));
+                disp('simulateMomentsMain: Time limit reached in parameter evaluation');
+                err('Time limit reached')
+                fileID5 = fopen('results/EEJKT_maxtime_error.txt','a');
+                fprintf(fileID5,'\r\n  ');
+                fprintf(fileID5,'\r\n sim time exceeds %.0f in simulateMomentsMain', mm.abort_time);
+                fprintf(fileID5,'\r\n firm type = %.2f', pt_ndx);
+                fprintf(fileID5,'\r\n params = ');
+                fprintf(fileID5,'\r%8.5f %8.5f %8.5f %8.5f %8.5f %8.5f',mm.param_vec(1:6));
+                fprintf(fileID5,'\r%8.5f %8.5f %8.5f %8.5f %8.5f %8.5f',mm.param_vec(7:end));
+                fprintf(fileID5, '\r\n  ');
+                fclose(fileID5);
+            end
 
-    rng(seeds(mm.pt_type(pt_ndx,1),1),'twister');
-    seed_crand(seeds(mm.pt_type(pt_ndx,1),2));
+            rng(seeds(mm.pt_type(pt_ndx,1),1),'twister');
+            seed_crand(seeds(mm.pt_type(pt_ndx,1),2));
 
-    if mm.sim_firm_num_by_prod_succ_type(pt_ndx)>0
+            if mm.sim_firm_num_by_prod_succ_type(pt_ndx)>0
 
-        [sim_out{pt_ndx}] = simulateForeignMatches(pt_ndx,macro_state_f, mm, policy);
+                [sim_out{pt_ndx}] = simulateForeignMatches(pt_ndx,macro_state_f, mm, policy);
 
-        [sim_out{pt_ndx}] = simulateHomeMatches(pt_ndx,macro_state_h, mm, policy,sim_out{pt_ndx});
+                [sim_out{pt_ndx}] = simulateHomeMatches(pt_ndx,macro_state_h, mm, policy,sim_out{pt_ndx});
 
-        sim_out{pt_ndx} = splice_hf(sim_out{pt_ndx},policy,mm,pt_ndx);
+                sim_out{pt_ndx} = splice_hf(sim_out{pt_ndx},policy,mm,pt_ndx);
 
+            end
+        end
     end
-
 end
- sim_time = toc(mm.start_time);
- fprintf('\r Simulation time: %.0f seconds\n', sim_time);
+sim_time = toc(mm.start_time);
+fprintf('\r Simulation time: %.0f seconds\n', sim_time);
 
 
 %% Uncomment commands below to generate data for spot checks
@@ -59,12 +64,12 @@ end
 % if sim_out{check_type}.domfirm_count > 0
 %   check_cell_H = sim_out{mm.check_type}.iterH_check;
 %   check_count_H = sim_out{mm.check_type}.stackH;
-%    save 'iter_out_HomeChecks.mat' 'check_type' 'check_cell_H' 'check_count_H';  
+%    save 'iter_out_HomeChecks.mat' 'check_type' 'check_cell_H' 'check_count_H';
 % end
 % if sim_out{check_type}.exptr_count >0
 %   check_cell_F = sim_out{mm.check_type}.iterF_check;
 %   check_count_F = sim_out{mm.check_type}.stackF;
-%  save 'iter_out_XptrChecks.mat' 'check_type' 'check_cell_F' 'check_count_F';  
+%  save 'iter_out_XptrChecks.mat' 'check_type' 'check_cell_F' 'check_count_F';
 % end
 
 % ALSO UNCOMMENT: lines 119-133 in simulateHomeMatchesInnerSim.m

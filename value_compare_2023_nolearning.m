dc9ec0345890a8e34567b2e8a3034cde98cdf176
veryshuai <dj.eco@cbs.dk>
2024-03-25 22:22:10 +0100

    added legend

--- value_compare_2023_nolearning.m
+++ value_compare_2023_nolearning.m
@@ -153,6 +153,7 @@ plot(x_vals,log(val_fail_f(:,3)));
 xlabel('Matches')
 ylabel('2023 USD (log scale)')
 title({'Known-theta','Value in foreign market'})
+legend({'Successes','Alternating', 'Failures'},'Location','east')
 hold off
 ax = gca();
 ax.YTickLabel = compose('%0.2g', exp(ax.YTick)');

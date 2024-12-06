ec3e587b60f2d80f1c71425a046a2f59e9985e94
veryshuai <dj.eco@cbs.dk>
2024-12-06 11:36:04 +0100

    added a save results so that moments are saved and can be plotted in the panel scatter plot

--- calculateDistanceAndPrint.m
+++ calculateDistanceAndPrint.m
@@ -12,6 +12,8 @@ err_comp = @(sta,fin) error(sta:fin)' * inv_W(sta:fin,sta:fin) * error(sta:fin);
 real_moms_and_sim_moms = cat(2,Data',Model);
 print_diagnostics_to_standard_output(D, X, real_moms_and_sim_moms, err_comp, simMoms,mm);
 
+save results/no_learning_moments
+
 plots_v2(simMoms); 
 summary_tables(simMoms,mm); 
 
